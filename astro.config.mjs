de67df87a7631f20df18d85921daab0b19c4cfff
Jonathan Ortega <jortega0389@gmail.com>
2026-05-17 21:23:54 -0500

    feat: add sanity dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss(), react()],
+  integrations: [tailwindcss(), react(), sanity()],
 
   vite: {
     plugins: [tailwindcss()],
